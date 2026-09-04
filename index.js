ffaac714767ce56cec88ab3f99dad824ee5a2db2
JhnRnz <grizzlee12345@gmail.com>
2026-09-04 10:41:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 import express from 'express';
 
-    let students = [{id: 0, name: "student0"}];
+let students = [
+    { id: 0, name: "Gelen" },
+    { id: 1, name: "Renzbert" },
+    { id: 2, name: "Dustin Jioily" },
+    { id: 3, name: "Clark" },
+    { id: 4, name: "Darwin" }
+];
 
 const app = express();
 
 app.use(express.json());
 
-
 app.get("/students", (req, res) => {
     res.send(students);
 });
- 
+
 app.post("/students", (req, res) => {
     const newStudent = req.body;
 
-    students =  [...students, newSrtudents];
+    students = [...students, newStudent];
 
     res.send(newStudent);
 });
 
-app.patch("/students/:name", (req, res) =>{
+app.patch("/students/:name", (req, res) => {
     const name = req.params.name;
     const updatedStudentData = req.body;
 
@@ -37,22 +42,16 @@ app.patch("/students/:name", (req, res) =>{
     };
 
     res.send(students[studentIndex]);
-
 });
 
 app.delete("/students/:id", (req, res) => {
-   const id = Number(req.params.id);
+    const id = Number(req.params.id);
 
-   students = students.filter((student) => student.id !== id);
+    students = students.filter((student) => student.id !== id);
 
-   res.send(students);  
-
+    res.send(students);
 });
 
 app.listen(3000, () => {
     console.log("listening to port 3000");
-
 });
-   
-
-
