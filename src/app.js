bda1134f9b1fc9121b417fed2e27bed6d2e95891
JhnRnz <grizzlee12345@gmail.com>
2026-09-04 08:50:30 +0800

    Update project

---
+++ src/app.js
@@ -0,0 +1,58 @@
+import express from 'express';
+
+    let students = [{id: 0, name: "student0"}];
+
+const app = express();
+
+app.use(express.json());
+
+
+app.get("/students", (req, res) => {
+    res.send(students);
+});
+ 
+app.post("/students", (req, res) => {
+    const newStudent = req.body;
+
+    students =  [...students, newSrtudents];
+
+    res.send(newStudent);
+});
+
+app.patch("/students/:name", (req, res) =>{
+    const name = req.params.name;
+    const updatedStudentData = req.body;
+
+    const studentIndex = students.findIndex(
+        (student) => student.name === name
+    );
+
+    if (studentIndex === -1) {
+        return res.status(404).send("Student not found");
+    }
+
+    students[studentIndex] = {
+        ...students[studentIndex],
+        ...updatedStudentData
+    };
+
+    res.send(students[studentIndex]);
+
+});
+
+app.delete("/students/:id", (req, res) => {
+   const id = Number(req.params.id);
+
+   students = students.filter((student) => student.id !== id);
+
+   res.send(students);  
+
+});
+
+//app.listen(3000, () => {
+    console.log("listening to port 3000");
+
+//});
+   
+
+
